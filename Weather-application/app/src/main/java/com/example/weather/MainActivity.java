a6a342bc77cd9508e93f4fd1ddffcbaba8d8fbcf
Neel Doshi <neeldoshi1477@gmail.com>
2021-08-14 14:01:58 +0530

    Update :+1:

--- Weather-application/app/src/main/java/com/example/weather/MainActivity.java
+++ Weather-application/app/src/main/java/com/example/weather/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    final String Api_Key = "63b7e10e976b5466d6b72957a54d36c0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
